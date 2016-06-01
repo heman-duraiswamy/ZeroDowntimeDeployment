5c128c5d453c7450f4b81bbc4a45e15d6c3ad111
Heman Duraiswamy <hduraiswamy@HW12333.home>
2016-05-31 23:13:21 -0400

    add more comments

--- scripts/generate_access_log_data.sh
+++ scripts/generate_access_log_data.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 
-
+# Check for reuired 5 parameters; else exit
 if [ $# -ne 5 ]; then
     echo "Invalid number of arguments."
     echo "Usage ./generate_access_log_data.sh <server_name> <app_name> <app_version> <is_more_error?(0|1)> <is_slow?(0|1)>"
     exit 1
 fi
 
+# Initialize variables
 SERVER_NAME=$1
 APP_NAME=$2
 APP_VERSION=$3
@@ -15,6 +16,7 @@ ERR_FLAG=0
 SLOW_FLAG=0
 SLEEP_DURATION=0.1
 
+# Ensure the variable 4 is either a 0 or 1; for all other values default it to 0
 temp0=`echo $4 | grep "^-\?[0-1]$"`
 if [ -z "$temp0" ]; then
     ERR_FLAG=0
@@ -22,6 +24,7 @@ else
     ERR_FLAG=$temp0
 fi
 
+# Ensure the variable 5 is either a 0 or 1; for all other values default it to 0
 temp1=`echo $5 | grep "^-\?[0-1]$"`
 if [ -z "$temp1" ]; then
     SLOW_FLAG=0
@@ -29,11 +32,12 @@ else
     SLOW_FLAG=$temp1
 fi
 
+# Perform infinite loop; ctrl+c to exit
 while true
 do
     # Calculate status code
     ## generate random number between 0 and 50 for normal state
-    ## generate random number between 1 and 20 for good state
+    ## generate random number between 1 and 20 for bad state
     if [ $ERR_FLAG -eq 0 ]; then 
         x1=`gshuf -i 1-50 -n 1`
     else
@@ -51,6 +55,8 @@ do
         status_code=200
     fi
 
+    # Calculate latency
+    ## generate random value between 0.100 seconds and 2.250 seconds for a normal state; times it 5 for bad state
     x2=`gshuf -i 100-2250 -n 1`
     if [ $SLOW_FLAG -eq 0 ]; then
         latency=`echo $(printf %0.3f $(echo "scale=3; $x2/1000" | bc))`
